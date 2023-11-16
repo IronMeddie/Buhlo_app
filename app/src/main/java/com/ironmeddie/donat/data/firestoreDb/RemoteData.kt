9dcdc0c7dd85189ba5d1df3669d342c278457894
taranyukm <mikaelofarius@gmail.com>
2023-11-16 14:08:31 +0300

    firestore refactor

--- app/src/main/java/com/ironmeddie/donat/data/firestoreDb/RemoteData.kt
+++ app/src/main/java/com/ironmeddie/donat/data/firestoreDb/RemoteData.kt
@@ -27,13 +27,13 @@ class RemoteData : RemoteDataBase {
 
             getCategorys().forEach {
                 val user = hashMapOf(
-                    "name" to it.name,
-                    "picture" to "",
-                    "amount" to "",
-                    "description" to "",
-                    "voutes" to ""
+                    NodesDocumetsFields.FIELD_NAME to it.name,
+                    NodesDocumetsFields.FIELD_PICTURE to "",
+                    NodesDocumetsFields.FIELD_AMOUNT to "",
+                    NodesDocumetsFields.FIELD_DESCRIPTION to "",
+                    NodesDocumetsFields.FIELD_VOUTES to ""
                 )
-                db.collection("alc_categories")
+                db.collection(NodesDocumetsFields.NODE_CATEGORY)
                     .add(user)
                     .await()
 
@@ -47,26 +47,26 @@ class RemoteData : RemoteDataBase {
     override suspend fun addTransaction(user: User, purchase: Double, categories: List<Category>) {
         try {
             val transaction = hashMapOf(
-                "email" to user.email,
-                "firstName" to user.firstName,
-                "dateTime" to FieldValue.serverTimestamp(),
-                "money" to purchase,
-                "categories" to categories.map { it.name }.toString()
+                NodesDocumetsFields.FIELD_EMAIL to user.email,
+                NodesDocumetsFields.FIELD_FIRSTNAME to user.firstName,
+                NodesDocumetsFields.FIELD_DATETIME to FieldValue.serverTimestamp(),
+                NodesDocumetsFields.FIELD_MONEY to purchase,
+                NodesDocumetsFields.FIELD_CATEGORIES to categories.map { it.name }.toString()
             )
-            db.collection("transactions")
+            db.collection(NodesDocumetsFields.NODE_TRANSACTIONS)
                 .add(transaction)
                 .await()
 
-            db.collection("money").document("money").update("money", FieldValue.increment(purchase))
+            db.collection(NodesDocumetsFields.NODE_MONEY).document(NodesDocumetsFields.DOCUMENT_MONEY).update(NodesDocumetsFields.FIELD_MONEY, FieldValue.increment(purchase))
                 .await()
 
             val amount = if (categories.size>0)purchase/categories.size else purchase
 
             categories.forEach {
-                db.collection("alc_categories").document(it.id).update("amount", FieldValue.increment(amount))
+                db.collection(NodesDocumetsFields.NODE_CATEGORY).document(it.id).update(NodesDocumetsFields.FIELD_AMOUNT, FieldValue.increment(amount))
                     .await()
 
-                db.collection("alc_categories").document(it.id).update("voutes", FieldValue.increment(1))
+                db.collection(NodesDocumetsFields.NODE_CATEGORY).document(it.id).update(NodesDocumetsFields.FIELD_VOUTES, FieldValue.increment(1))
                     .await()
             }
 
@@ -86,8 +86,8 @@ class RemoteData : RemoteDataBase {
 
     override fun getCurrentMoney(): Flow<Money> = flow {
         try {
-            val money = db.collection("money")
-                .document("money")
+            val money = db.collection(NodesDocumetsFields.NODE_MONEY)
+                .document(NodesDocumetsFields.DOCUMENT_MONEY)
                 .get()
                 .await().toObject<Money>()
             emit(money ?: Money())
@@ -104,15 +104,15 @@ class RemoteData : RemoteDataBase {
         return flow<List<Category>> {
             try {
                 val list: List<Category> =db
-                    .collection("alc_categories")
+                    .collection(NodesDocumetsFields.NODE_CATEGORY)
                     .get()
                     .await().map {
                         Category(
                             id = it.id,
-                            name = it.data["name"].toString(),
-                            picture = it.data["picture"].toString(),
-                            amount = it.data["amount"].toString().toFloat(),
-                            voutes = it.data["voutes"].toString()
+                            name = it.data[NodesDocumetsFields.FIELD_NAME].toString(),
+                            picture = it.data[NodesDocumetsFields.FIELD_PICTURE].toString(),
+                            amount = it.data[NodesDocumetsFields.FIELD_AMOUNT].toString().toFloat(),
+                            voutes = it.data[NodesDocumetsFields.FIELD_VOUTES].toString()
                         )
                     }
                 emit(list)
@@ -130,16 +130,16 @@ class RemoteData : RemoteDataBase {
         return flow<List<Transaction>> {
             try {
                 val list = db
-                    .collection("transactions")
+                    .collection(NodesDocumetsFields.NODE_TRANSACTIONS)
                     .get()
                     .await().map {
 
-                        val cat = it.data["categories"].toString().toListOfStrings()
+                        val cat = it.data[NodesDocumetsFields.FIELD_CATEGORIES].toString().toListOfStrings()
                         Transaction(
-                            userName = it.data["firstName"].toString(),
-                            email = it.data["email"].toString(),
-                            dateTime = (it.data["dateTime"] as Timestamp).toTimeFormat(),
-                            money = it.data["money"].toString(),
+                            userName = it.data[NodesDocumetsFields.FIELD_FIRSTNAME].toString(),
+                            email = it.data[NodesDocumetsFields.FIELD_EMAIL].toString(),
+                            dateTime = (it.data[NodesDocumetsFields.FIELD_DATETIME] as Timestamp).toTimeFormat(),
+                            money = it.data[NodesDocumetsFields.FIELD_MONEY].toString(),
                             categories = cat,
                             id = it.id,
                         )
@@ -154,4 +154,34 @@ class RemoteData : RemoteDataBase {
 
         }
     }
+}
+
+object NodesDocumetsFields{
+
+    // nodes
+    const val NODE_CATEGORY = "alc_categories"
+    const val NODE_TRANSACTIONS = "transactions"
+    const val NODE_MONEY = "money"
+
+    //documents
+    const val DOCUMENT_MONEY = "money"
+
+    // fields
+    const val FIELD_MONEY = "money"
+//    const val FIELD_APPROVED = "approved"
+
+
+    const val FIELD_CATEGORIES = "categories"
+    const val FIELD_DATETIME = "dateTime"
+    const val FIELD_EMAIL = "email"
+    const val FIELD_FIRSTNAME = "firstName"
+
+    const val FIELD_AMOUNT = "amount"
+    const val FIELD_VOUTES = "voutes"
+    const val FIELD_PICTURE = "picture"
+    const val FIELD_DESCRIPTION = "description"
+    const val FIELD_NAME = "name"
+
+
+
 }
